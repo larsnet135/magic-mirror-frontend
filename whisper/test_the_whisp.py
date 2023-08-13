bd2094804f38ae8f05cf795c0acacc628d29551d
Demir Catovic <dcatovic@amazon.com>
2023-08-13 17:40:56 +0200

    add Alice speaking something

--- whisper/test_the_whisp.py
+++ whisper/test_the_whisp.py
@@ -7,3 +7,6 @@ print(result_1["text"])
 
 result_2 = model.transcribe("audio_files/audio_2.mp3")
 print(result_2["text"])
+
+result_3 = model.transcribe("audio_files/audio_3.mp3")
+print(result_3["text"])
