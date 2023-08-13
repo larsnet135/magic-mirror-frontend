027888436ae14caf358f961c3c635c74ce4fb2af
Demir Catovic <dcatovic@amazon.com>
2023-08-13 17:37:19 +0200

    refactor to accomodate two simple examples

---
+++ whisper/test_the_whisp.py
@@ -0,0 +1,9 @@
+import whisper
+
+model = whisper.load_model("tiny")
+
+result_1 = model.transcribe("audio_files/audio_1.mp3")
+print(result_1["text"])
+
+result_2 = model.transcribe("audio_files/audio_2.mp3")
+print(result_2["text"])
