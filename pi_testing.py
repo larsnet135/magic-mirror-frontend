bc1b09ae735e6c184538f8d804e22a26174f4270
Lars Nettemann <lars.nettemann@web.de>
2023-08-16 09:57:47 +0200

    added testing py file

---
+++ pi_testing.py
@@ -0,0 +1,16 @@
+import time
+
+def main():
+    messages = ["Starting the script...", 
+                "Waiting for a while...", 
+                "Printing again...", 
+                "And we wait again...", 
+                "Final message!", 
+                "Exiting the script..."]
+
+    for msg in messages:
+        print(msg)
+        time.sleep(2)  # Waits for 2 seconds
+
+if __name__ == "__main__":
+    main()
